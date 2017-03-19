729afa06fd39330503bd7c3e5a733a1e13a9d9b5
Silvio Wangler <silvio.wangler@gmail.com>
2017-03-19 11:04:35 +0100

    API Route

--- server/src/main/groovy/ch/silviowangler/timer/verticles/HttpServerVerticle.groovy
+++ server/src/main/groovy/ch/silviowangler/timer/verticles/HttpServerVerticle.groovy
@@ -2,8 +2,11 @@ package ch.silviowangler.timer.verticles
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.logging.LoggerFactory
+import io.vertx.ext.auth.jwt.JWTAuth
 import io.vertx.ext.web.Router
-
+import io.vertx.ext.web.handler.CookieHandler
+import io.vertx.ext.web.handler.SessionHandler
+import io.vertx.ext.web.sstore.LocalSessionStore
 /**
  * @author Silvio Wangler
  */
@@ -20,15 +23,53 @@ class HttpServerVerticle extends AbstractVerticle {
                 logActivity: true
         ]
 
+        def config = [
+                keyStore: [
+                        path    : "/Users/saw/development/private/timer/server/keystore.jceks",
+                        type    : "jceks",
+                        password: "secret"
+                ]
+        ]
+
         def server = vertx.createHttpServer(options)
+        def provider = JWTAuth.create(vertx, config)
 
         def router = Router.router(vertx)
 
-        router.route().handler({ routingContext ->
+        router.route().handler(CookieHandler.create())
+        router.route().handler(
+                SessionHandler.create(LocalSessionStore.create(vertx))
+                        .setCookieHttpOnlyFlag(true)
+                        .setCookieSecureFlag(true)
+        )
 
+        //router.route().path('/api/*').handler(BasicAuthHandler.create(provider))
+
+        def apiRoute = router.route().path('/*')
+
+        apiRoute.handler({ context ->
+            context.response()
+            // do not allow proxies to cache the data
+                    .putHeader("Cache-Control", "no-store, no-cache")
+            // prevents Internet Explorer from MIME - sniffing a
+            // response away from the declared content-type
+                    .putHeader("X-Content-Type-Options", "nosniff")
+            // Strict HTTPS (for about ~6Months)
+                    .putHeader("Strict-Transport-Security", "max-age=" + 15768000)
+            // IE8+ do not allow opening of attachments in the context of this resource
+                    .putHeader("X-Download-Options", "noopen")
+            // enable XSS for IE
+                    .putHeader("X-XSS-Protection", "1; mode=block")
+            // deny frames
+                    .putHeader("X-FRAME-OPTIONS", "DENY")
+
+            context.next()
+        })
+
+        def routeRecords = router.route().path('/api/user/records')
+
+        routeRecords.handler({ routingContext ->
             def request = routingContext.request()
-
-            logger.info("Request: ${request.uri()}")
 
             request.bodyHandler({ b ->
                 println "REQUEST: ${b.toString()}"
@@ -42,12 +83,14 @@ class HttpServerVerticle extends AbstractVerticle {
                         // Write to the response and end it
                         response.end(reply.result().body())
                     } else {
-                        response.end("Scheisse!")
+                        response.end("Shit that did not work out!")
                     }
                 })
             })
+        })
 
-
+        router.route().handler({ context ->
+            context.response().putHeader("content-type", "text/plain").end('Hello visitor')
         })
 
         server.requestHandler(router.&accept).listen()
